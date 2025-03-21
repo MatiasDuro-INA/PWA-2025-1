7d580d5cf235c89bfe8552bd4424e886f0ab1581
Matias <matias.duro@ina.edu.ar>
2025-03-20 22:01:36 -0300

    Clase 1 terminada

---
+++ Clase-1/Arrays.js
@@ -0,0 +1,142 @@
+let frutas = ["Banana", "Manzana", "Frutilla"]
+
+// console.log(frutas);
+
+// push: agrega un elemento al final del array
+
+frutas.push("Mango")
+
+// console.log(frutas);
+
+// pop: elimina el ultimo elemento del array
+frutas.pop()
+
+// console.log(frutas);
+
+
+// unshift: agrega un elemento al comienzo del array
+frutas.unshift("Uvas")
+
+// console.log(frutas);
+
+// shift: remueve el primer elemento del array
+frutas.shift()
+
+// console.log(frutas);
+
+
+// Map: Va a recorrer cada elemento del array, los va a transformar uno por uno,  
+// y devuelve un nuevo array, segun la funcion que elijamos
+let numeros = [1, 2, 3, 4, 5, 6, 7]
+
+// let nuevosNumeros2 = numeros.map(num => num + 5)
+// console.log(nuevosNumeros2);
+
+let nuevosNumeros = numeros.map(num => {
+    return num * num
+})
+
+// console.log(numeros);
+
+
+// console.log(nuevosNumeros);
+
+
+// Filter: Filtra los elementos, 1 por 1, segun la condicion, y devuelve el nuevo array
+
+let mayores = numeros.filter(numero => numero <= 5)
+// console.log(mayores);
+
+
+let Carrito = [
+    {
+        nombre: "Tostadora",
+        cantidad: 2,
+        precio: 2000
+    },
+    {
+        nombre: "Cafetera",
+        cantidad: 3,
+        precio: 1500
+    },
+    {
+        nombre: "TV",
+        cantidad: 1,
+        precio: 2000
+    }
+]
+// Reduce: reduce el array a un solo valor, acumulandolo en un acumulador
+
+let cantidadCarrito = Carrito.reduce((acumulador, item) => {
+    return acumulador + item.cantidad
+}, 0)
+
+let totalCarrito = Carrito.reduce((acumulador, item) => {
+    return acumulador + (item.precio * item.cantidad)
+}, 0)
+
+// console.log("Cantidad en el carrito: ", cantidadCarrito);
+// console.log("Monto total en el carrito: ", totalCarrito);
+
+
+// find: Devuelve el PRIMER elemento que cumpla con la condicion
+let busqueda = numeros.find( num => num > 5)
+
+// console.log(busqueda);
+
+// Some: Devuelve true si al menos un elemento, cumple con la condicion
+let existeEl6 = numeros.some( num => num == 8)
+
+// console.log("Existe este numero?: ",existeEl6);
+
+//Every: Devuelve true si TODOS los elementos, cumplen con la condicion
+let todos = numeros.every( num => num < 7)
+
+// console.log("Existe este numero?: ",todos);
+
+//Join: Convierte el array a string, y los separa a cada elemento con el separador que asignemos
+
+
+let numerosNuevos = numeros.join("///")
+
+// console.log(numerosNuevos);
+// console.log(numeros);
+
+// Da vuelta el array
+// numeros.reverse()
+
+// console.log(numeros);
+
+//Splice: modifica el array eliminando y reemplazando por otro los elementos que querramos
+// Y retorna el elemento eliminado
+
+console.log(numeros);
+
+let eliminados = numeros.splice(5, 2, "Peras", "Duraznos")
+
+// console.log(numeros);
+
+// console.log(eliminados);
+
+// Concat: Devuelve una concatenacion de un array con otro
+
+let ArrayConcatenado = numeros.concat(frutas)
+
+console.log(ArrayConcatenado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Clase-1/Objecto.js
+++ Clase-1/Objecto.js
@@ -1,5 +1,79 @@
-// Comentario
 
-const user = {
-    nombre: "Diego"
+// Funcion
+
+// nombre funcion = function("parametros"){
+//    Contenido de la funcion
+// } 
+
+let mensaje
+let saludo  = function(){
+    // console.log("Iniciando funcion...");
+    
+    let mensaje = "Hola como estan"
+    // console.log("Mensaje: ", mensaje);
+    // console.log("Mensaje1: ", mensaje);
+
+    return mensaje
+    // return "Hola como estan"
+}
+// console.log("Mensaje2: ", mensaje);
+
+let SaludarPersona = (amigo) => {
+
+    let saludo = "Hola "
+
+    // console.log("Variable Amigo es: ", amigo);
+    
+
+    return saludo + amigo
+}
+
+let amigo = "Evelyn"
+
+let materias = {
+    PWA: {
+        activa: true,
+        nota: 10,
+        profesores: {
+            nombre: "Matias"
+        }
+    },
+    Programacion1: {
+        activa: true,
+        nota: null
+    }
+}
+
+const usuario = {
+    nombre: "Matias",
+    activo: true,
+    saludo: saludo,
+    SaludarPersona,
+    materias
+}
+
+// console.log("Usuario Saluda a: ", usuario.SaludarPersona(amigo));
+
+console.log("Materias del usuario: ", usuario.materias.PWA.profesores);
+
+
+
+
+
+// console.log("Saludo va a ser: ", mensaje);
+
+
+
+// Formas de declarar una funcion
+
+// Funcion Standart
+const sum = function(a, b){
+    return a + b
+}
+
+sum(20, 5)
+
+// Funcion Flecha
+const func2 = () => {
+
 }
